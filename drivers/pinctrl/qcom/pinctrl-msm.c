42495038faefe97ca34d0b2482382e1eca571775
Jainuddin Shaik <quic_jshaik@quicinc.com>
2025-03-26 17:04:20 +0530

    Set irq affinity for tti gpio
    - Used irq_force_affinity to affine tti irqs to core 3.
    - Applied upstream patch to fix kernel hung issue:
      "genirq: Get rid of global lock in irq_do_set_affinity()"
      with commit: 64b6d1d7a84538de34c22a6fc92a7dcc2b196b64
    
    Change-Id: I4bf925d24554c6ef13fc7fdbef430f379fb611fb
    CRs-Fixed: 4096929
    Signed-off-by: Jainuddin Shaik <quic_jshaik@quicinc.com>

--- drivers/pinctrl/qcom/pinctrl-msm.c
+++ drivers/pinctrl/qcom/pinctrl-msm.c
@@ -1206,15 +1206,8 @@ static int msm_gpio_irq_set_affinity(struct irq_data *d,
 	struct gpio_chip *gc = irq_data_get_irq_chip_data(d);
 	struct msm_pinctrl *pctrl = gpiochip_get_data(gc);
 
-#if 1
-	if (d->parent_data && test_bit(d->hwirq, pctrl->skip_wake_irqs))
-		return irq_chip_set_affinity_parent(d, dest, force);
-
-	return -EINVAL;
-#else
-	irq_set_affinity_hint(pctrl->irq, dest);
+	irq_force_affinity(pctrl->irq, dest);
 	return IRQ_SET_MASK_OK;
-#endif
 }
 
 static int msm_gpio_irq_set_vcpu_affinity(struct irq_data *d, void *vcpu_info)
